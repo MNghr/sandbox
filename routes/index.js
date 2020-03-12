ddf31bcd2b8920c61bc8a948079f62943be8e0f5
MNghr <nghrmty@gmail.com>
2020-03-12 14:23:08 +0900

    スレッド機能，投稿機能の実装終了

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,15 @@ let moment = require("moment");
 let connection = require("../mysqlConnection");
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let query = 'SELECT *,DATE_FORMAT(created_at, \'%Y年%m月%d日 %k時%i分%s秒\') AS created_at FROM boards';
+  connection.query(query, function (err, rows) {
+    console.log(rows);
+    res.render('index', {
+      title: '掲示板っぽいやつ',
+      boardList: rows
+    });
+  });
+  //res.render('index', { title: 'Express' });
 });
 
 router.post('/', function (req, res, next) {
