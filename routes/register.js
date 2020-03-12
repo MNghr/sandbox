eedd522c537b9a829b9fa401505c9d0616ec4d8f
MNghr <nghrmty@gmail.com>
2020-03-12 16:31:06 +0900

    email重複時の警告を実装

--- routes/register.js
+++ routes/register.js
@@ -14,10 +14,21 @@ router.post('/', function (req, res, next) {
     let email = req.body.email;
     let password = req.body.password;
     let createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
-    let query = 'INSERT INTO users (user_name,email,password,created_at) VALUES ("' + userName + '", "' + email + '", "' + password + '" , "' + createdAt + '")';
+    let emailExistsQuery = 'SELECT * FROM users WHERE email = "' + email + '"LIMIT 1';
+    let registerQuery = 'INSERT INTO users (user_name,email,password,created_at) VALUES ("' + userName + '", "' + email + '", "' + password + '" , "' + createdAt + '")';
     console.log(query);
-    connection.query(query, function (err, rows) {
-        res.redirect('/login');
+    connection.query(emailExistsQuery, function (err, email) {
+        let emailExists = email.length;
+        if (emialExists) {
+            res.render('register', {
+                title: "sign up",
+                emialExists: "そのemailアドレスは既に使われています"
+            });
+        }else{
+            connection.query(registerQuery, function (err, rows) {
+                res.redirect('/login');
+            });
+        }
     });
 });
 
